024dedff55e4446f5ba8871de1b00a8fd0e44b11
sama8940 <no56guti@gmail.com>
2025-08-12 13:52:57 +0900

    aaa

--- GuiApp.py
+++ GuiApp.py
@@ -22,5 +22,50 @@ class GuiApp:
         except:
             print(self.__class__.__name__ +
                   ': Use defalut value(s)' , file=sys.stderr)
-            root.geometry(self.__class__.__name__ +
-                  '+' + clientX + '+' + clientY)
+        root.geometry(cliientWidth  + 'x'  + clientHeight +'+' + clientX + '+' + clientY)
+        root.option_add(' *tearOff' , tkinter.FALSE)
+        menu = tkinter.Menu()
+        menuFIle = tkinter.Menu()
+        menu.add_cascade(menu=menuFIle, label= 'ファイル(F)', underline=5)
+        menuFIle.add_command(label='終了', underline=3,
+            command=self.menuFileExit, accelerator='ALT+F4')
+        menuHelp = tkinter.Menu()
+        menu.add_cascade(menu=menuHelp, label='ヘルプ(H)', underline=4)
+        menuHelp.add_command(label='ヘルプファイルを開く(O)',
+            underline=10, command=self.menuHelpOpenWeb)
+        menuHelp.add_command(label='バージョン情報(V)',
+            underline=8, command=self.menuFileExit)
+        root['menu'] = menu
+        root.protocol('WM_DELETE_WINDOW', self.menuFileExit)
+
+def menuFileExit(self, event=None):
+    cp = configparser.ConfigParser()
+    cp['Client'] = {
+        'X': str(root.winfo_x()),
+        'Y': str(root.winfo_y()),
+        'Height': str(root.winfo_height()),
+        'Width': str(root.winfo_width())
+    }
+    with open(self.iniFilename, 'w') as f:
+        cp.write(f)
+    root.destroy()
+
+def menuHelpOpenWeb(self):
+    helpFilePath = os.path.dirname(__file__) + os.sep + 'help.thm'
+    if os.path.isfile(helpFilePath):
+        helpFIlePath = 'fille:///' + helpFIlePath.replase(os.sep, '/')
+        webbrowser.open(helpFilePath)
+    else:
+        messagebox.showerror(self.____class__.__name__,
+            helpFIlePath    + 'がありません')
+
+def menuHelpVersion(self):
+    s = self.__class__.__name__
+    s += ' Verson 0.01(2023/06/01)\n'
+    s += '@2023 Hideo Harada\n'
+    s += 'with Python ' + sys.version
+    messagebox.showinfo(self.__class__.__name__, s)
+
+root = tkinter.Tk()
+GuiApp(root)
+root.mainloop()
