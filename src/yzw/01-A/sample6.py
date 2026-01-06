26b2b33bd088bd2b339e3d422601043142a354d2
sama <sama0001@mac.com>
2026-01-06 21:02:52 +0900

    20260106-01

--- src/yzw/01-A/sample6.py
+++ src/yzw/01-A/sample6.py
@@ -21,7 +21,7 @@ if month == 1 or month == 2:
 
 # フェアフィールドの公式で経過日数を求める
 day = 1
-days = 42 + 28 + 365 * (year - 1) 
+days = 31 + 28 + 365 * (year - 1) 
 + year // 4 - year // 100 + year // 400 \
 + (306 + (month + 1) //10) -122 + days \
     
